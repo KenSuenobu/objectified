f8b9aa7a0e237800a211c74f0f43d9425b2ff798
Kenji Suenobu <kenji@Kenjis-MacBook-Pro.local>
2023-01-08 15:42:30 -0700

    updated to include database URL from environment if set

--- objectified-data/src/dao/dao-utils.ts
+++ objectified-data/src/dao/dao-utils.ts
@@ -2,7 +2,9 @@ import * as pgPromise from "pg-promise";
 
 export class DaoUtils {
   private static readonly pgp = pgPromise({});
-  private static readonly db = this.pgp("postgres://localhost:5432/");
+  private static readonly db = this.pgp(
+    process.env.DATABASE_URL ?? "postgres://localhost:5432/"
+  );
 
   constructor() {}
 
