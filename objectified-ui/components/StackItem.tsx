94e9b4bf3361967465172012aead50cdfef4e323
Ken Suenobu <ksuenobu@fastmail.com>
2023-02-09 22:13:01 -0700

    updated to include namespace flow with an error message.

--- objectified-ui/components/StackItem.tsx
+++ objectified-ui/components/StackItem.tsx
@@ -3,5 +3,4 @@ import Box from '@mui/material/Box';
 
 export const StackItem = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-  textAlign: 'center',
 }));
--- objectified-ui/pages/namespaces/index.tsx
+++ objectified-ui/pages/namespaces/index.tsx
@@ -1,15 +1,29 @@
 import {NextPage} from 'next';
 import {Stack} from '@mui/system';
 import ListHeader from '../../components/ListHeader';
-import {useState} from 'react';
+import {useRef, useState} from 'react';
 import LoadingMessage from '../../components/LoadingMessage';
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField} from '@mui/material';
+import { StackItem } from '../../components/StackItem';
 
 const Namespaces: NextPage = () => {
   const [namespaces, setNamespaces] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [addNamespaceShowing, setAddNamespaceShowing] = useState(false);
+  const [errorState, setErrorState] = useState<[boolean, string]>([false, '']);
+  const namespaceRef = useRef();
 
-  const addNamespaceClicked = () => {
-    console.log('Namespace clicked.');
+  const addNamespaceClicked = () => setAddNamespaceShowing(true);
+
+  const addNamespace = () => {
+    const namespaceValue = namespaceRef?.current?.value ?? '';
+
+    if (namespaceValue.length > 0) {
+      console.log(`Add namespace: ${namespaceValue}`);
+      setAddNamespaceShowing(false);
+    } else {
+      setErrorState([true, 'Namespace is missing a value.']);
+    }
   }
 
   if (loading) {
@@ -20,6 +34,31 @@ const Namespaces: NextPage = () => {
 
   return (
     <>
+      <Dialog open={errorState[0]}>
+        <DialogTitle>Error</DialogTitle>
+        <DialogContent>
+          <Stack direction={'row'}>
+            {errorState[1]}
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setErrorState([false, errorState[1]])}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog open={addNamespaceShowing}>
+        <DialogTitle>Namespace</DialogTitle>
+        <DialogContent>
+          <Stack direction={'row'}>
+            <StackItem sx={{ width: '100%' }}>
+              <TextField id={'namespace'} label={'Namespace'} variant={'outlined'} required inputRef={namespaceRef}/>
+            </StackItem>
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={addNamespace}>Add</Button>
+          <Button onClick={() => setAddNamespaceShowing(false)}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
       <Stack direction={'row'}>
         <ListHeader header={'Namespaces'} onAdd={addNamespaceClicked}/>
       </Stack>
