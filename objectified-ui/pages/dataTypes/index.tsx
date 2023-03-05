fbc20812b8b77d21b55eb586be25d70e883dbe41
Ken Suenobu <ksuenobu@fastmail.com>
2023-03-04 23:54:50 -0700

    save work

--- objectified-ui/pages/dataTypes/index.tsx
+++ objectified-ui/pages/dataTypes/index.tsx
@@ -1,10 +1,10 @@
 import { NextPage } from "next";
 import {
-  Button,
+  Button, Checkbox,
   Dialog,
   DialogActions,
   DialogContent,
-  DialogTitle, FormControl, InputLabel, Select, Table, TableBody, TableCell,
+  DialogTitle, FormControl, FormControlLabel, InputLabel, Select, Table, TableBody, TableCell,
   TableContainer, TableHead, TableRow,
   TextField,
   Typography
@@ -12,15 +12,23 @@ import {
 import {Box, Stack} from '@mui/system';
 import {StackItem} from '../../components/StackItem';
 import {CheckBox, CheckBoxOutlineBlank, Delete} from '@mui/icons-material';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import axios from 'axios';
 import LoadingMessage from '../../components/LoadingMessage';
 import MenuItem from '@mui/material/MenuItem';
+import {errorDialog} from '../../components/dialogs/ConfirmDialog';
 
 const DataTypes: NextPage = () => {
   const [loading, setLoading] = useState(true);
   const [dataTypes, setDataTypes] = useState([]);
   const [addDataTypeShowing, setAddDataTypeShowing] = useState(false);
+  const nameRef = useRef<HTMLInputElement>();
+  const descriptionRef = useRef<HTMLInputElement>();
+  const maxLengthRef = useRef<HTMLInputElement>();
+  const patternRef = useRef<HTMLInputElement>();
+  const enumValuesRef = useRef<HTMLInputElement>();
+  const enumDescriptionsRef = useRef<HTMLInputElement>();
+  const examplesRef = useRef<HTMLInputElement>();
 
   const reloadDataTypes = () => {
     setLoading(true);
@@ -36,8 +44,22 @@ const DataTypes: NextPage = () => {
     setAddDataTypeShowing(true);
   }
 
-  const addDataType = () => {
+  const validate = () => {
+    errorDialog('Missing value.');
+    return false;
+  }
 
+  const addDataType = () => {
+    if (validate()) {
+      const name = nameRef.current?.value ?? '';
+      const description = descriptionRef.current?.value ?? '';
+      const maxLength = maxLengthRef.current?.value ?? '0';
+      const pattern = patternRef.current?.value ?? '';
+      const enumValues = enumValuesRef.current?.value ?? '';
+      const enumDescriptions = enumDescriptionsRef.current?.value ?? '';
+      const examples = examplesRef.current?.value ?? '';
+      
+    }
   }
 
   useEffect(() => {
@@ -53,62 +75,72 @@ const DataTypes: NextPage = () => {
   return (
     <>
       <div sx={{ width: '100%' }} style={{ border: '1px solid #ddd' }}>
-        <Dialog open={addDataTypeShowing}>
+        <Dialog open={addDataTypeShowing} fullWidth>
           <DialogTitle>Data Type</DialogTitle>
           <DialogContent>
             <Stack direction={'column'}>
 
               <StackItem sx={{ width: '100%' }}>
-                {/*<TextField id={'namespace'} label={'Namespace'} variant={'outlined'} required inputRef={namespaceRef}/>*/}
-                <TextField id={'name'} label={'Name'} variant={'outlined'} required fullWidth/>
+                <TextField id={'name'} label={'Name'} variant={'outlined'} required
+                           fullWidth inputRef={nameRef}/>
               </StackItem>
 
               <StackItem sx={{ width: '100%' }}>
-                <TextField id={'description'} label={'Description'} variant={'outlined'} required fullWidth/>
+                <TextField id={'description'} label={'Description'} variant={'outlined'}
+                           required fullWidth inputRef={descriptionRef}/>
               </StackItem>
 
               <StackItem sx={{ width: '100%' }}>
-                <FormControl fullWidth>
-                  <InputLabel id={'data-type-label'}>Data Type</InputLabel>
-                  <Select labelId={'data-type-label'} id={'data_type'} label={'Data Type'}>
-                    <MenuItem>STRING</MenuItem>
-                    <MenuItem>INT32</MenuItem>
-                    <MenuItem>INT64</MenuItem>
-                    <MenuItem>FLOAT</MenuItem>
-                    <MenuItem>DOUBLE</MenuItem>
-                    <MenuItem>BOOLEAN</MenuItem>
-                    <MenuItem>DATE</MenuItem>
-                    <MenuItem>DATE_TIME</MenuItem>
-                    <MenuItem>BYTE</MenuItem>
-                    <MenuItem>BINARY</MenuItem>
-                    <MenuItem>PASSWORD</MenuItem>
-                    <MenuItem>OBJECT</MenuItem>
-                  </Select>
-                </FormControl>
+                <Stack direction={'row'}>
+                  <StackItem sx={{ width: '20%' }}>
+                    <FormControlLabel control={<Checkbox/>} label={'Array of: '}/>
+                  </StackItem>
+
+                  <StackItem sx={{ width: '80%' }}>
+                    <FormControl fullWidth>
+                      <InputLabel id={'data-type-label'}>Data Type</InputLabel>
+                      <Select labelId={'data-type-label'} id={'data_type'} label={'Data Type'}>
+                        <MenuItem>STRING</MenuItem>
+                        <MenuItem>INT32</MenuItem>
+                        <MenuItem>INT64</MenuItem>
+                        <MenuItem>FLOAT</MenuItem>
+                        <MenuItem>DOUBLE</MenuItem>
+                        <MenuItem>BOOLEAN</MenuItem>
+                        <MenuItem>DATE</MenuItem>
+                        <MenuItem>DATE_TIME</MenuItem>
+                        <MenuItem>BYTE</MenuItem>
+                        <MenuItem>BINARY</MenuItem>
+                        <MenuItem>PASSWORD</MenuItem>
+                        <MenuItem>OBJECT</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </StackItem>
+                </Stack>
               </StackItem>
 
               <StackItem sx={{ width: '100%' }}>
-                is_array BOOLEAN NOT NULL DEFAULT false,
+                <TextField id={'max_length'} label={'Maximum Input Length'} variant={'outlined'}
+                           required fullWidth inputRef={maxLengthRef}/>
               </StackItem>
 
               <StackItem sx={{ width: '100%' }}>
-                max_length INT NOT NULL DEFAULT 0,
+                <TextField id={'pattern'} label={'Regexp Pattern'} variant={'outlined'}
+                           required fullWidth inputRef={patternRef}/>
               </StackItem>
 
               <StackItem sx={{ width: '100%' }}>
-                pattern TEXT,
+                <TextField id={'enum_values'} label={'Enumeration Values'} variant={'outlined'}
+                           required fullWidth inputRef={enumValuesRef}/>
               </StackItem>
 
               <StackItem sx={{ width: '100%' }}>
-                enum_values TEXT[],
+                <TextField id={'enum_descriptions'} label={'Enumeration Descriptions'} variant={'outlined'}
+                           required fullWidth inputRef={enumDescriptionsRef}/>
               </StackItem>
 
               <StackItem sx={{ width: '100%' }}>
-                enum_descriptions TEXT[],
-              </StackItem>
-
-              <StackItem sx={{ width: '100%' }}>
-                examples TEXT[],
+                <TextField id={'examples'} label={'Examples'} variant={'outlined'}
+                           required fullWidth inputRef={examplesRef}/>
               </StackItem>
             </Stack>
           </DialogContent>
