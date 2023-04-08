6d36d86dbbfe1633e865f871fd6b58c4f57a01d0
Ken Suenobu <ksuenobu@fastmail.com>
2023-04-07 21:37:22 -0600

    added more functionality to the array manager

--- objectified-ui/components/common/ArrayManager.tsx
+++ objectified-ui/components/common/ArrayManager.tsx
@@ -1,6 +1,6 @@
 import {StackItem} from "../StackItem";
-import {Button, TextField, Typography} from "@mui/material";
-import React from "react";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Typography} from "@mui/material";
+import React, {useRef, useState } from "react";
 import {Stack} from "@mui/system";
 import {AddOutlined} from "@mui/icons-material";
 
@@ -11,8 +11,30 @@ export interface ArrayManagerProps {
 }
 
 const ArrayManager = (props: ArrayManagerProps) => {
+  const [open, setOpen] = useState(false);
+  const itemValueRef = useRef();
+
+  const addItem = () => {
+    props.objectArray.push(itemValueRef.current.value);
+    setOpen(false);
+  }
+
   return (
     <>
+      <Dialog open={open} maxWidth={'sm'} fullWidth>
+        <DialogContent>
+          <Stack direction={'column'}>
+            <StackItem sx={{ width: '100%', padding: '4px' }}>
+              <TextField id={'item_value'} label={'Item Value'} variant={'outlined'} required inputRef={itemValueRef} fullWidth/>
+            </StackItem>
+          </Stack>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => addItem()} variant={'contained'}>Add</Button>
+          <Button onClick={() => setOpen(false)} variant={'contained'} color={'error'}>Cancel</Button>
+        </DialogActions>
+      </Dialog>
+
       <Stack direction={'row'}>
         <StackItem sx={{ width: '80%', textAlign: 'left', backgroundColor: '#ddd' }}>
           <Typography fontWeight={'bold'} sx={{ color: 'black', verticalAlign: 'middle', padding: '1em' }}>
@@ -20,9 +42,11 @@ const ArrayManager = (props: ArrayManagerProps) => {
           </Typography>
         </StackItem>
         <StackItem sx={{ width: '20%', textAlign: 'right', backgroundColor: '#ddd', paddingRight: '1em', paddingTop: '10px'  }}>
-          <Button variant={'contained'}>Add</Button>
+          <Button variant={'contained'} onClick={() => setOpen(true)}>Add</Button>
         </StackItem>
       </Stack>
+
+      {props.objectArray.toString()}
     </>
   );
 }
