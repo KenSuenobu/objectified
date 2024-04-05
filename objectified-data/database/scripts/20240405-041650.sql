55abf9f25e5e444dcf83698191a7eff5b3386f83
Ken Suenobu <ksuenobu@fastmail.com>
2024-04-04 22:41:02 -0600

    added classes

---
+++ objectified-data/database/scripts/20240405-041650.sql
@@ -0,0 +1,67 @@
+DROP TABLE IF EXISTS obj.class CASCADE;
+DROP INDEX IF EXISTS idx_class_unique_name;
+
+CREATE TABLE obj.class (
+    id SERIAL NOT NULL PRIMARY KEY,
+    namespace_id INT NOT NULL REFERENCES obj.namespace(id),
+    owner_id INT NOT NULL REFERENCES obj.user(id),
+    name VARCHAR(80) NOT NULL,
+    description VARCHAR(4096) NOT NULL,
+    enabled BOOLEAN NOT NULL DEFAULT true,
+    create_date TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
+    update_date TIMESTAMP WITHOUT TIME ZONE,
+    delete_date TIMESTAMP WITHOUT TIME ZONE
+);
+
+CREATE UNIQUE INDEX idx_class_unique_name ON obj.class(namespace_id, UPPER(name));
+
+---
+
+DROP TYPE IF EXISTS obj.data_type_enum CASCADE;
+CREATE TYPE obj.data_type_enum AS ENUM (
+    'STRING', 'INT32', 'INT64', 'FLOAT', 'DOUBLE', 'BOOLEAN', 'DATE', 'DATE_TIME',
+    'BYTE', 'BINARY', 'PASSWORD', 'OBJECT'
+);
+
+DROP TABLE IF EXISTS obj.data_type CASCADE;
+DROP INDEX IF EXISTS idx_data_type_unique_name;
+
+CREATE TABLE obj.data_type (
+    id SERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(80) NOT NULL,
+    description VARCHAR(4096) NOT NULL,
+    data_type obj.data_type_enum NOT NULL,
+    is_array BOOLEAN NOT NULL DEFAULT false,
+    max_length INT NOT NULL DEFAULT 0,
+    pattern TEXT,
+    enum_values TEXT[],
+    enum_descriptions TEXT[],
+    examples TEXT[],
+    enabled BOOLEAN NOT NULL DEFAULT true,
+    core_type BOOLEAN NOT NULL DEFAULT false,
+    create_date TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
+    update_date TIMESTAMP WITHOUT TIME ZONE,
+    delete_date TIMESTAMP WITHOUT TIME ZONE
+);
+
+CREATE UNIQUE INDEX idx_data_type_unique_name ON obj.data_type(UPPER(name));
+
+---
+
+DROP TABLE IF EXISTS obj.field CASCADE;
+DROP INDEX IF EXISTS idx_field_unique_name;
+
+CREATE TABLE obj.field (
+    id SERIAL NOT NULL PRIMARY KEY,
+    namespace_id INT NOT NULL REFERENCES obj.namespace(id),
+    data_type_id INT NOT NULL REFERENCES obj.data_type(id),
+    name VARCHAR(80) NOT NULL,
+    description VARCHAR(4096) NOT NULL,
+    default_value TEXT,
+    enabled BOOLEAN NOT NULL DEFAULT true,
+    create_date TIMESTAMP WITHOUT TIME ZONE DEFAULT NOW(),
+    update_date TIMESTAMP WITHOUT TIME ZONE,
+    delete_date TIMESTAMP WITHOUT TIME ZONE
+);
+
+CREATE UNIQUE INDEX idx_field_unique_name ON obj.field(namespace_id, UPPER(name));
